817a3a7add0d2555bca81c869aedeea0f269b4e8
hunghd <hunghd.yb@gmail.com>
2021-10-18 23:44:31 +0700

    Updates

---
+++ precache-manifest.24b548b0a1fb5f8035c84163bdc8f50a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a502ff651cae680398f99138e71cd23f",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "a8669c2963fbd45e31ea",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "d74018fed2872d5117f4",
+    "url": "/home/static/js/2.11e38c3f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.11e38c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8669c2963fbd45e31ea",
+    "url": "/home/static/js/main.438ece2a.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "5b3adaa7aae2c300113ef69f896d2985",
+    "url": "/home/static/media/hunghd.5b3adaa7.jpg"
+  },
+  {
+    "revision": "b155118612bec9bc1e0354e8d278acc4",
+    "url": "/home/static/media/resume.b1551186.pdf"
+  }
+]);
